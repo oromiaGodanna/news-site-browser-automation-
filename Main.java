0875dfc6b010ac401e4a88f31f2c27773d0f52bf
oromiaGodanna <37736629+oromiaGodanna@users.noreply.github.com>
2018-06-26 22:35:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,64 @@
+package com.company;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        method1();
+    }
+    public static void method1(){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ori\\IdeaProjects\\browserAutomationNewsWebsite\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.dailynews.co.zw/articles/news");
+        WebElement leadTitle =  driver.findElement(By.className("lead"));
+        List <WebElement> otherTitles= driver.findElements(By.className("article-row"));
+        otherTitles.add(0, leadTitle);
+
+           /*System.out.println(leadTitle.getText());
+
+        for(WebElement eachElement:otherTitles){
+            System.out.println(eachElement.getText());
+        }*/
+        forward(otherTitles);
+
+        System.out.println("page Title: " + driver.getTitle());
+        System.out.println("Current URl: " + driver.getCurrentUrl());
+        System.out.println("Length of page Source: " + driver.getPageSource().toString().length());
+
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+
+        }
+
+        driver.close();
+    }
+    public static void forward(List<WebElement> otherTitles){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ori\\IdeaProjects\\autotest\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("localhost:8000");
+
+       // WebElement allDiv = driver.findElement(By.id("all"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        for(WebElement eachElement:otherTitles){
+            String eachDiv = eachElement.getText();
+            //js.executeScript("var node = document.createElement('DIV');");
+//            js.executeScript("var textNode = document.createTextNode('this is the frist div that i am gonna create');");
+//            js.executeScript("node.appendChild(textNode);");
+//            js.executeScript("document.getElementById('all').appendChild(node);");
+
+        }
+
+
+    }
+}
